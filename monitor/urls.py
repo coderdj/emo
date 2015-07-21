3fd411d4cec2bde321b158f73a9806fe86832a37
Daniel Coderre <coderdj@gmail.com>
2015-07-21 18:07:23 +0200

    Updates to make runs display compatible with new doc format

--- monitor/urls.py
+++ monitor/urls.py
@@ -6,9 +6,10 @@ from monitor import views
 urlpatterns = [
     
     url(r'^eventdisplay', login_required(TemplateView.as_view(template_name="monitor/eventdisplay.html"))),
-    url(r'^pax_display', login_required(TemplateView.as_view(template_name="monitor/pax_display.html"))),
+    url(r'^pax_display', login_required(TemplateView.as_view(template_name="monitor/pax_display.html"))),    
     url(r'^waveforms', login_required(TemplateView.as_view(template_name="monitor/waveformdisplay.html"))),
     url(r'^diagnostics', login_required(TemplateView.as_view(template_name="monitor/diagnostics.html"))),
+    url(r'^runstats', login_required(TemplateView.as_view(template_name="monitor/runstats.html"))),
     url(r'^get_aggregate_list', login_required(views.get_aggregate_list), name="get_aggregate_list"),
     url(r'^get_aggregate_plot', login_required(views.get_aggregate_plot), name="get_aggregate_plot"),
     url(r'^get_event_for_display', login_required(views.get_event_for_display), name="get_event_for_display"),
@@ -16,5 +17,6 @@ urlpatterns = [
     url(r'^noise_report', login_required(views.get_noise_spectra), name="get_noise_spectra"),
     url(r'^noise', login_required(views.noise_directory), name="noise_directory"),
     url(r'^get_event_as_json', login_required(views.get_event_as_json), name="get_event_as_json"),
-
-]
+    url(r'^get_calendar_events', login_required(views.get_calendar_events),
+        name='get_calendar_events'),
+ ]
