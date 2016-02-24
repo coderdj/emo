87ec4c52f626c548fc1190f85a76cdf80cd3b908
Daniel Coderre <coderdj@gmail.com>
2016-02-24 19:16:47 +0100

    Updates upon migration

--- emo/wsgi.py
+++ emo/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emo.settings")
 
 application = get_wsgi_application()
