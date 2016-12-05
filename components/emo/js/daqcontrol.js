10313e013db3d4530b3911238395b31125d39ce7
Daniel Coderre <coderdj@gmail.com>
2016-12-05 18:46:18 +0100

    Actually fixed issue from last commit

--- components/emo/js/daqcontrol.js
+++ components/emo/js/daqcontrol.js
@@ -272,9 +272,9 @@ return html;
 
 }
 function GetElapsedTime(startTimeString){
+    startTimeString+="Z";
     var currentDate = new Date().getTime();
     var startDate = Date.parse(startTimeString);
-
     var seconds = Math.abs(currentDate - startDate)/1000;
     var hours = Math.floor(seconds/3600);
     var minutes = Math.floor((seconds - (hours*3600) ) /60);
